6a30a77622ac9f049f139780c5ef3d99165ed301
Dheer-Rajpoot <rajpoot.dheer@gmail.com>
2013-10-03 09:56:29 -0700

    Upload Multiple Files using ASP.NET 4.5

---
+++ MultipleFileUpload/FileUpload.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FileUpload.aspx.cs" Inherits="FileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="MultipleFileUpload" runat="server" AllowMultiple="true" />
+        <br />
+        <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ MultipleFileUpload/FileUpload.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class FileUpload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (MultipleFileUpload.HasFiles)
+        {
+            StringBuilder UploadedFileNames = new StringBuilder();
+
+            foreach (HttpPostedFile uploadedFile in MultipleFileUpload.PostedFiles)
+            {
+                string FileName = HttpUtility.HtmlEncode(Path.GetFileName(uploadedFile.FileName));
+                uploadedFile.SaveAs(System.IO.Path.Combine(Server.MapPath("~/Files/"), FileName));
+                UploadedFileNames.AppendFormat("{0}<br />", FileName);
+            }
+            Response.Write("Uploaded Files are : <br/>" + UploadedFileNames);
+        }
+    }
+
+}
